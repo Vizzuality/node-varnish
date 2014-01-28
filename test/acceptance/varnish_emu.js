36ae51cd28b0ca4cacba135dfa8ddce5e7342788
Tiago Relvao <relvao@gmail.com>
2014-01-28 13:23:48 -0500

    Added support for authentication  - Fix EventEmitter inheritance  - Logging is now optional: client.debug = true;# with '#' will be ignored, and an empty message aborts the commit.  - "ready_callback" now gets 2 params: err and clientInstance

--- test/acceptance/varnish_emu.js
+++ test/acceptance/varnish_emu.js
@@ -1,24 +1,40 @@
 var net = require('net');
 
-function VarnishEmu(on_cmd_recieved, port) {
-    var self = this;
-    var welcome_msg = 'hi, im a varnish emu, right?';
+function VarnishEmu(on_cmd_recieved, port, requireAuth) {
+    var self = this,
+        msg = '', 
+        code = 0;
 
     self.commands_recieved = [];
-
     var sockets = [];
     var server = net.createServer(function (socket) {
-      var command = '';
-      socket.write("200 " + welcome_msg.length + "\n");
-      socket.write(welcome_msg);
-      socket.on('data', function(data) {
-        self.commands_recieved.push(data);
-        server.commands++;
-        on_cmd_recieved && on_cmd_recieved(self.commands_recieved);
-        socket.write('200 0\n');
-      });
-      sockets.push(socket);
+        if(requireAuth){
+            msg = 'bkiehgjkhonrgtlzvikyvynswrinerqf';
+            code = 107;
+        } else {
+            msg = 'hi, im a varnish emu, right?';
+            code = 200;
+        }
+        socket.write(code + " " + msg.length + "\n");
+        socket.write(msg);
+        socket.on('data', function(data) {
+            self.commands_recieved.push(data);
+            server.commands++;
+            on_cmd_recieved && on_cmd_recieved(self.commands_recieved);
+            if(requireAuth){
+                if(data.toString('utf8') === 'auth 1ae1b9b5069b151d5ad5ee3339a33dc5321b5fa7ad2c31709d21490ba90e6a09\n'){
+                    socket.write('200 0\n');
+                }else{
+                    socket.write(code + " " + msg.length + "\n");
+                    socket.write(msg);
+                }
+            }else{
+                socket.write('200 0\n');
+            }
+        });
+        sockets.push(socket);
     });
+    
     server.commands = 0;
     server.listen(port || 0, "127.0.0.1");
     server.close_connections = function() {
@@ -30,4 +46,3 @@ function VarnishEmu(on_cmd_recieved, port) {
 }
 
 module.exports = VarnishEmu;
-
